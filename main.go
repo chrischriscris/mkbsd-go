9b4af352e7678d80f25ce44c82a53259d9e841dc
chrischriscris <18-10892@usb.ve>
2024-09-29 21:37:32 -0400

    base read urls

--- main.go
+++ main.go
@@ -6,55 +6,93 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
 )
 
 const URL = "https://storage.googleapis.com/panels-api/data/20240916/media-1a-i-p~s"
 
 type ImageUrl struct {
-    Dhd string
-    Dsd string
-    S string
+	Dhd string
+	Dsd string
+	S   string
+	E   string
 }
 
 type PanelResponse struct {
-    Version int
-    Data map[int]ImageUrl
+	Version int
+	Data    map[int]ImageUrl
 }
 
-func readToJson(url string) error {
-    res, err := http.Get(url)
-    if err != nil {
-        return fmt.Errorf("Couldn't visit %s", url)
-    }
+func readToJson(url string) ([]string, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't visit %s", url)
+	}
 
-    b, err := io.ReadAll(res.Body)
-    if err != nil {
-        return fmt.Errorf("Couldn't read response body. Url: %s", url)
-    }
-    // sb := string(body)
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't read response body. Url: %s", url)
+	}
 
-    var f PanelResponse
-    err = json.Unmarshal(b, &f)
-    if err != nil {
-        return fmt.Errorf("Couldn't read json from response body. Url: %s", url)
-    }
+	var f PanelResponse
+	err = json.Unmarshal(b, &f)
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't read json from response body. Url: %s", url)
+	}
 
-    for k, v := range f.Data {
-        if v == nil {
-            fmt.Println("Element %d has no ...")
-            continue
-        }
-        fmt.Printf("Element %d: %s\n", k, v.Dhd)
-    }
+	var arr []string
+	for _, v := range f.Data {
+		if v.Dhd != "" {
+			arr = append(arr, v.Dhd)
+			continue
+		}
 
-    return nil
+		if v.Dsd != "" {
+			arr = append(arr, v.Dsd)
+			continue
+		}
+
+		if v.S != "" {
+			arr = append(arr, v.S)
+			continue
+		}
+
+		if v.E != "" {
+			arr = append(arr, v.E)
+			continue
+		}
+	}
+
+	return arr, nil
 }
 
+func getFileName(s string) (string, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return "", fmt.Errorf("Failed to parse URL: %s", s)
+	}
+
+	return path.Base(u.Path), nil
+}
 
 func main() {
-    err := readToJson(URL)
-    if err != nil {
-        log.Fatalf("Error: %s", err)
-    }
+	urls, err := readToJson(URL)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+    fmt.Println(urls)
+	for _, s := range urls {
+		fmt.Printf("Parsing url: %s\n", s)
+		name, err := getFileName(s)
+		if err != nil {
+		    fmt.Fprintf(os.Stderr, "Error: %s", err)
+            continue
+		}
+
+        fmt.Printf("Name: %s\n", name)
+	}
 
 }
